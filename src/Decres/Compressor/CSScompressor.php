12657bf350eeb2ed23b288fe8b226b33861b945c
WouterJ <waldio.webdesign@gmail.com>
2012-06-18 17:00:51 +0200

    Created CSScompressor

---
+++ src/Decres/Compressor/CSScompressor.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This is a file in the Decres project.
+ * The Decres project is released under a MIT
+ * license.
+ *
+ * @copyright 2012 - Wouter J
+ * @version 1.0
+ */
+
+
+namespace Decres\Compressor;
+
+class CSScompressor extends CompressorInterface
+{
+    public function compress($str)
+    {
+    }
+}
